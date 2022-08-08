75269f38db00706a669dec7ec2f1c176a0d917bc
matheusbrito07 <matheusrj.dev@gmail.com>
2022-08-08 10:57:17 -0300

    Criando ambiente fe, criando componentes funcionais do todo app.

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader'
+export default props => (
+  <div>
+    <PageHeader name='Sobre' small='Nós'></PageHeader>
+    <h2>História</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipis</p>
+    <h2>Missão</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipis</p>
+    <h2>Visão</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipis</p>
+  </div>
+)
