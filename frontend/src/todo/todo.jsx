06fd9f8fa71ba09bc9608acb3c40887b8cffe759
matheusbrito07 <matheusrj.dev@gmail.com>
2022-08-08 16:32:59 -0300

    Criando constructor, funçoes add, select, remove,  e metodo de refresh do todo App

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,15 +1,75 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const URL = 'http://localhost:3003/api/todos'
 export default class Todo extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { description: '', list: [] }
+    
+    this.handleChange = this.handleChange.bind(this)
+    this.handleAdicionar = this.handleAdicionar.bind(this)
+    
+    this.handleRemover = this.handleRemover.bind(this)
+    this.handleConcluido = this.handleConcluido.bind(this)
+    this.handlePendente = this.handlePendente.bind(this)
+
+    this.refresh()
+  }
+
+  refresh() {
+    axios.get(`${URL}?sort=-createdAt`)
+      .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+  }
+
+  handleChange(e){
+    this.setState({...this.state, description: e.target.value })
+  }
+
+  handleAdicionar(){
+    const description = this.state.description
+    axios.post(URL, {description})
+      .then(resp => this.refresh())
+  }
+
+  handleRemover(todo){
+    axios.delete(`${URL}/${todo._id}`)
+      .then(resp => this.refresh())
+  }
+
+  handleConcluido(todo) {
+    axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+      .then(resp => this.refresh())
+  }
+
+  handlePendente(todo) {
+    axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+    .then(resp => this.refresh())
+  }
+
   render() {
     return (
       <div>
         <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-        <TodoForm />
-        <TodoList />
+
+        <TodoForm 
+        description={this.state.description}
+        handleChange={this.handleChange}
+        handleAdicionar={this.handleAdicionar}
+        />
+
+        <TodoList 
+        list={this.state.list}
+        handleRemover={this.handleRemover}
+        handleConcluido={this.handleConcluido}
+        handlePendente={this.handlePendente}
+        />
+
       </div>
     )
   }
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Grid from '../template/grid'
+import IconButton from "../template/iconButton";
 
 export default props => (
-  <div>
-    <h1>Form</h1>
+  <div role='form' className='todoForm'>
+    <Grid className='12 9 10'>
+      <input 
+      id='description' 
+      className='form-control' 
+      placeholder='Adicione uma tarefa'
+      value={props.description}
+      onChange={props.handleChange}
+      /> 
+    </Grid>
+
+    <Grid className='12 3 2'>
+      <IconButton 
+      style='primary' 
+      icon='plus'
+      onClick={props.handleAdicionar}
+      />
+    </Grid>
+
   </div>
 )
